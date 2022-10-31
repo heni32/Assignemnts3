aa684567b0d5f53915c4375038358a4564d97d00
heni32 <sp.heni32@yahoo.com>
2022-10-30 22:18:45 -0500

    Added lab assignement 4

---
+++ app/src/main/java/com/miu/assignemnts/MainActivityLab4.kt
@@ -0,0 +1,25 @@
+package com.miu.assignemnts
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivityLab4  : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layoutlab4)
+
+
+
+
+
+
+
+    }
+
+    fun click(view: View) {
+
+
+    }
+}
---
+++ app/src/main/java/com/miu/assignemnts/User.kt
@@ -0,0 +1,5 @@
+package com.miu.assignemnts
+
+class User(val userName: String, var passWord: String)  {
+
+}
